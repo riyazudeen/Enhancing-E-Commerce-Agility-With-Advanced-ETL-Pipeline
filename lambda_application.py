40f6f48e065db70779f7ddaedaca69d77c28b083
Riyazudeen <36462741+riyazudeen@users.noreply.github.com>
2024-09-23 21:08:01 +0530

    Update lambda_application.py

--- lambda_application.py
+++ lambda_application.py
@@ -3,11 +3,13 @@ import boto3
 
 s3_client = boto3.client('s3')
 glue_client = boto3.client('glue')
+step_client = boto3.client('stepfunctions')
 
-ORDER_BUCKET = 'flipdummyreturned'
-RETURNS_BUCKET = 'flipedummyorders'
+ORDER_BUCKET = 'Enter your order budget name'
+RETURNS_BUCKET = 'Enter your return budget name'
 
 def lambda_handler(event, context):
+    
     # Check if new file is uploaded in one of the buckets
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     
@@ -15,11 +17,20 @@ def lambda_handler(event, context):
     order_files = check_s3_bucket_for_files(ORDER_BUCKET)
     returns_files = check_s3_bucket_for_files(RETURNS_BUCKET)
 
+    input_data = {
+        'file1': f"s3://{ORDER_BUCKET}/ordertable.csv",
+        'file2': f"s3://{RETURNS_BUCKET}/returntable.csv"  # Replace with the second file's key
+    }
+
     if order_files and returns_files:
         # If files exist in both, trigger Glue ETL job
         try:
-            response = glue_client.start_job_run(JobName='glue etl')
+            #response = glue_client.start_job_run(JobName='order_return_etl')
+            step_response = step_client.start_execution(
+        stateMachineArn="Enter your arn step function",
+    )
             print(f"Glue job started: {response['JobRunId']}")
+            print(f"Glue job started: {step_response}")
         except Exception as e:
             print(f"Error starting Glue job: {str(e)}")
     else:
